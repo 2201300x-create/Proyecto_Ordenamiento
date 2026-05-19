f8aed7488fb229f1f435b750cbea380e079890df
2201300x-create <2201300x@umich.mx>
2026-05-19 13:38:45 -0600

    main.c completo

--- src/main.c
+++ src/main.c
@@ -10,11 +10,100 @@ int size[] = { 100, 200, 300, 400, 500, 1000, 2500, 5000, 7000};
 int num_sizes = 9;
 
 // Nombres para las celdas del CSV
-const char* dist_names[] = {"uniform", "..."};
+const char* dist_names[] = {"uniform", "ordenado", "reverso", "casi", "duplicados"};
 const char* algo_name[] = {"Insertion", "Merge", "Counting", "Introsort"};
 // Propiedad para la estabilidad de cada uno
 const char* estable[] = {"SI", "SI", "SI", "NO"};
 
 int main() {
+	// Abrir/Crear el archivo CSV
+	FILE *fp = fopen("resultado.csv", "w");
+	if (fp == NULL) {
+		printf("ERROR: No se pudo crear el archivo CSV.\n");
+		return 1;
+	}
 
+	// Escribir la cabecera del CSV
+	fprintf(fp, "algoritmo,tamaño,distribucion,repeticion,time_ms,comparaciones,swaps,estable\n");
+
+	// Semilla para los numeros aleatorios
+	srand(time(NULL));
+
+	// Bucle 1: Tamaño
+	for (int s = 0; s < num_sizes; s++) {
+		int n = sizes[s];
+		int *base_arr = (int *)malloc(n * sizeof(int));
+		int *test_arr = (int *)malloc(n * sizeof(int));
+
+		// Bucle 2: Distribucion
+		for (int d = 0; d < 5; d++) {
+			// Bucle 3: Repeticiones de minimo 3
+			for (int rep = 1; rep <= 3; rep++) {
+				// Generar el arreglo base segun la distribucion
+				switch(d) {
+					case 0:
+						generar_uniform(base_arr, n);
+						break;
+					case 1:
+						generar_ordenamado(base_arr, n);
+						break;
+					case 2:
+						generar_reverso(base_arr, n);
+						break;
+					case 3:
+						generar_casi(base_arr, n);
+						break;
+					case 4:
+						generar_duplicado(base_arr, n);
+						break;
+				}
+
+				// Bucle 4: Algoritmos de 0 a 3
+				for (int a = 0; a < 4; a++) {
+					// Copiar el arreglo para que todos los algoritmos exactamente lo mismo
+					memcpy(test_arr, base_arr, n * sizeof(int));
+
+					long long comps = 0;
+					long long swaps = 0;
+					clock_t start, end;
+					double cpu_time_used;
+
+					// Iniciar cronometro y ejecutar algoritmo
+					start = clock();
+					switch (a) {
+						case 0:
+							insertion_sort(test_arr, n, &comps, &swaps);
+							break;
+						case 1:
+							merge_sort(test_arr, 0, n, -1, &comps, &swaps);
+							break;
+						case 2:
+							counting_sort(test_arr, n, &comps, &swaps);
+							break;
+						case 3:
+							introsort(test_arr, n, &comps, &swaps);
+							break;
+					}
+					end = clock();
+
+					// Calcular el tiempo en ms
+					cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC * 1000.0;
+
+					// Escribir en el CSV
+					if (a == 2) {
+						// Counting Sort no usa comparaciones
+						fprintf(fp, "%s,%d,%s,%d,%.4f,NA,%lld,%s\n", algo_names[a], n, dist_names[d], rep, cpu_time_used, swaps, estable[a]);
+					} else {
+						fprintf(fp, "%s,%d,%s,%d,%.4f,%lld,%lld,%s\n", algo_names[a], n, dist_names[d], rep, cpu_time_used, comps, swaps, estable[a]);
+					}					
+				}
+			}
+		}
+		fre(base_arr);
+		fre(test_arr);
+		printf("Completado tamaño: %d\n", n);
+	}
+	fclose(fp);
+    printf("\n [INFO]: Resultados guardados en 'resultados.csv'.\n");
+    return 0;
 }

72aa7f3bfd3d6bfa2ab51219ae438d74fba978f4
2201300x-create <2201300x@umich.mx>
2026-05-18 18:37:16 -0600

    Archivos .h subidos, junto con la estructura de los .c

---
+++ src/algoritmos.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "algoritmos.h"
+
+// 1. Insertion Sort O(n^2)
+void insertion_sort(int arr[], int n, long long *comps, long long *swaps){
+
+}
+
+// 2. Counting Sort O(n) - Lineal
+void counting_sort(int arr[], int n, long long *comps, long long *swaps){
+
+}
+
+// 3. Merge Sort O(n log n) - Estable
+void merge_sort(int arr[], int l, int r, long long *comps, long long *swaps){
+
+}
+
+// 4. Introsort (Hibrido) - Inestable
+void introsort(int arr[], int n, long long *comps, long long *swaps){
+
+}
---
+++ src/datasets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "datasets.h"
+
+void generar_uniform(int arr[], int n){
+
+}
+
+void generar_ordenado(int arr[], int n){
+
+}
+
+void generar_reverso(int arr[], int n){
+	
+}
+
+void generar_casi(int arr[], int n){
+
+}
+
+void generar_duplicados(int arr[], int n){
+
+}

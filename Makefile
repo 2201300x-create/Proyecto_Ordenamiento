0139b7c5fde6789ae29275cb6d4bc90e85beebee
2201300x-create <2201300x@umich.mx>
2026-05-20 15:05:01 -0600

    Resubidos

--- Makefile
+++ Makefile
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CC = gcc
 CFLAGS = -Wall -O2
 
@@ -14,20 +13,3 @@ run: all
 
 clean:
 	rm -f $(OUT)
-=======
-CC = gcc
-CFLAGS = -Wall -O2
-
-SRC = src/main.c src/algoritmos.c src/datasets.c
-
-OUT = benchmark
-
-all:
-	$(CC) $(CFLAGS) $(SRC) -o $(OUT) -lm
-
-run: all
-	./$(OUT)
-
-clean:
-	rm -f $(OUT)
->>>>>>> 4da7289b6b90241c3e623e771430cd8791e88de2
--- graficar.py
+++ graficar.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -39,29 +38,3 @@ plt.savefig("grafica_tiempos.png")
 
 # Mostrar gráfica
 plt.show()
-=======
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# Leer CSV
-df = pd.read_csv("resultados.csv")
-
-# Obtener algoritmos únicos
-algoritmos = df["algoritmo"].unique()
-
-# ===== Tiempo =====
-plt.figure(figsize=(10,6))
-
-for alg in algoritmos:
-    sub = df[df["algoritmo"] == alg]
-    plt.plot(sub["n"], sub["time_ms"], marker='o', label=alg)
-
-plt.xlabel("Tamaño del arreglo (n)")
-plt.ylabel("Tiempo (ms)")
-plt.title("Tiempo de ejecución")
-plt.legend()
-plt.grid()
-
-plt.savefig("grafica_tiempos.png")
-plt.show()
->>>>>>> 4da7289b6b90241c3e623e771430cd8791e88de2
